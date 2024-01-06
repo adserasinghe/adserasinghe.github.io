87ef0eaa52ba48758f409698def5fdf599e6ae63
S.V.Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2024-01-06 15:40:14 +0530

    Bug Fixed Performance Enhanched...

--- About/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-   $('.yearJS') .text(new Date().getFullYear());
-});
-
-window.addEventListener('DOMContentLoaded', (event)=>{
-    document.querySelector('.yearJS').innerHTML=new Date().getFullYear();
-})
