f4d6ad7014c412ae2520f7705e4538fb914d8067
S.V.Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2024-02-18 22:52:01 +0530

    V:1.0.6.3 -> 1.0.6.5 *Portfolio Section Changed... *New Portfolio projects added... *Bug Fixed...

--- JS/carushal.js
+++ JS/carushal.js
@@ -1,18 +1,44 @@
-const carousel = document.querySelector('.carousel');
-const slides = document.querySelectorAll('.slide');
+const gallery = document.querySelector('.gallery');
+const track = document.querySelector('.gallery-track');
+const cards = document.querySelectorAll('.card');
+const easing = 0.05;
+let startY = 0;
+let endY = 0;
+let raf;
 
-let counter = 0;
+const lerp = (start,end,t) => start * (1-t) + end * t;
 
-setInterval(() => {
-  carousel.style.transition = 'transform 0.5s ease';
-  carousel.style.transform = `translateX(-${counter * 100}%)`;
-  counter++;
+function updateScroll() {
+  startY = lerp(startY,endY,easing);
+  gallery.style.height = `${track.clientHeight}px`;
+  track.style.transform = `translateY(-${startY}px)`;
+  activateParallax();
+  raf = requestAnimationFrame(updateScroll);
+  if (startY.toFixed(1) === window.scrollY.toFixed(1)) cancelAnimationFrame(raf);
+}
 
-  if (counter === slides.length) {
-    setTimeout(() => {
-      carousel.style.transition = 'none';
-      counter = 0;
-      carousel.style.transform = `translateX(-${counter * 100}%)`;
-    }, 500);
-  }
-}, 2000);
+function startScroll() {
+  endY = window.scrollY; 
+  cancelAnimationFrame(raf);
+  raf = requestAnimationFrame(updateScroll);
+}
+
+function parallax(card) {
+  const wrapper = card.querySelector('.card-image-wrapper');
+  const diff = card.offsetHeight - wrapper.offsetHeight;
+  const {top} = card.getBoundingClientRect();
+  const progress = top / window.innerHeight;
+  const yPos = diff * progress;
+  wrapper.style.transform = `translateY(${yPos}px)`;
+}
+
+const activateParallax = () => cards.forEach(parallax);
+
+function init() {
+  activateParallax();
+  startScroll();
+}
+
+window.addEventListener('load',updateScroll,false);
+window.addEventListener('scroll',init,false);
+window.addEventListener('resize',updateScroll,false);
