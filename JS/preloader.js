98c9adc489ba1f8f4babc8bfb9248fc3415dd00c
Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2024-11-10 08:58:22 +0530

    new preloader added

---
+++ JS/preloader.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        document.querySelector('.preloader').style.display = 'none';
+    }, 100); // Adjust timeout as needed
+});
