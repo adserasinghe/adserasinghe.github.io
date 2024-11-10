a23d7aebc2903375d56d0185e5808300e1b96908
Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2024-11-10 09:02:16 +0530

    some bugs fixed in preloader

--- JS/preloader.js
+++ JS/preloader.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
     setTimeout(function() {
         document.querySelector('.preloader').style.display = 'none';
-    }, 100); // Adjust timeout as needed
+    }, 2000); // Adjust timeout as needed
 });
